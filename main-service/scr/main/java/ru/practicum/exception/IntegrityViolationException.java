6c47048c9833b59fc4f7b6af337fcb0de8e67fb5
Ksenia <kseniiabelka05@mail.ru>
2024-11-14 23:25:33 +0300

    Дипломный проект 2-ая часть

---
+++ main-service/scr/main/java/ru/practicum/exception/IntegrityViolationException.java
@@ -0,0 +1,8 @@
+
+package ru.practicum.exception;
+
+public class IntegrityViolationException extends RuntimeException {
+    public IntegrityViolationException(String message) {
+        super(message);
+    }
+}
